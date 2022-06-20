9a405545e870ec3aa605af0e8055b784b9dcd583
chaeBrian <bmichelibanez@gmail.com>
2022-06-20 16:39:10 -0300

    proyecto final

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,5 @@
 let tbody = document.querySelector('.tbody');
 let shopCart = [];
-//API Premier League (Table)
-let matches = document.getElementById("matches");
-let urlApi = "https://api-football-standings.azharimm.site/leagues/eng.1/standings?season=2021&sort=asc";
-fetch(urlApi)
-.then(res => res.json())
-.then((json) => {
-    for (const club of json.data.standings){
-        let trApi = document.createElement("tr");
-        trApi.innerHTML = `
-            <td class="premierPosition">${club.stats[8].displayValue}</td>
-            <td class="premierClub d-flex flex-row"><img class="img-fluid" src="${club.team.logos[0].href}" alt=""><span>${club.team.shortDisplayName}</span></td>
-            <td class="premierStats">${club.stats[3].displayValue}</td>
-            <td class="premierStats">${club.stats[0].displayValue}</td>
-            <td class="premierStats">${club.stats[2].displayValue}</td>
-            <td class="premierStats">${club.stats[1].displayValue}</td>
-            <td class="premierStats">${club.stats[4].displayValue}</td>
-            <td class="premierStats">${club.stats[5].displayValue}</td>
-            <td class="premierStats">${club.stats[9].displayValue}</td>
-            <td class="premierStats--modifier">${club.stats[6].displayValue}</td>
-        `
-        document.getElementById("matches").append(trApi);
-    }
-});
 //Evento click sobre el Add to Cart.
 const buttonClick = document.querySelectorAll('.button');
 buttonClick.forEach(btn => { btn.addEventListener('click', addToCartItem)});
@@ -109,8 +86,8 @@ function removeItemShopCart(e){
     Toastify({
         text: "Jersey removed from Bag",
         offset: {
-          x: 10, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
-          y: -3 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+          x: 10,
+          y: -3 
         },
         style: {
             background: "linear-gradient(to right, #e90053, #e90053)",
@@ -152,3 +129,124 @@ window.onload = function(){
         renderCart();
     }
 }
+//Countdown Timer
+// establezco la fecha en la que estamos contando hacia atrás
+let countDownDate = new Date("Aug 5, 2022 16:00:00").getTime();
+// actualizo la cuenta regresiva cada 1 segundo
+let x = setInterval(function() {
+  // obtengo la fecha y hora de hoy
+  let now = new Date().getTime();
+  // encuentro la distancia entre ahora y la fecha de la cuenta regresiva
+  let distance = countDownDate - now;
+  // cálculos de tiempo para días, horas, minutos y segundos.
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // muestra el resultado en un elemento con id="timer"
+  document.getElementById("timer").innerHTML =`
+    <a class="border border-warning text-warning p-2 fw-bold fs-5 text-decoration-none" 
+        href="https://www.premierleague.com/news/2646764">
+        Fixtures Here
+    </a>
+    <span class="fs-2 fw-bolder text-warning">Premier League 2022-23 season starts in</span>
+    <div class="d-flex flex-row border border-light m-2">
+        <p class="m-1"><span class="fs-6 fw-bold text-light">${days}</span><br>DAYS</p>
+        <p class="m-1"><span class="fs-6 fw-bold text-light">${hours}</span><br>HRS</p>
+        <p class="m-1"><span class="fs-6 fw-bold text-light">${minutes}</span><br>MIM</p>
+        <p class="m-1"><span class="fs-6 fw-bold text-light">${seconds}</span><br>SEC</p>
+    </div>
+    `
+  // aviso para cuando termina la cuenta regresiva
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "Premier League 22/23 is underway";
+  }
+}, 1000);
+//Api local
+const premierLeagueNews = async () => {
+    let responseNews = await fetch("./premier-league-news.json")
+    let dataNews = await responseNews.json()
+    for (const premierNews of dataNews){
+        let newsContainer = document.createElement('div');
+        newsContainer.classList.add('newsPremierLeague');
+        newsContainer.innerHTML = `
+            <a target="_blank" href="${premierNews.newsLink}" class="d-flex flex-row m-2" style="border: 1px solid rgba(0, 0, 0, 0.211);">
+                <img class="w-25" src="${premierNews.newsImg}">
+                <div class="d-flex flex-column p-2">
+                    <p class="" style="font-size: smaller; font-weight: bold; color: #6b0d76; font-style: italic;">${premierNews.newsCatg}</p>
+                    <p class="ms-3" style="color: #3b0541;">${premierNews.newsTittle}</p>
+                </div>
+            </a>
+        `
+        document.getElementById("premierLeagueNewsId").append(newsContainer)
+    }
+}
+const premierLeagueClubs = async () => {
+    let responseClubs = await fetch("./premier-league-clubs.json")
+    let dataClubs = await responseClubs.json()
+    console.log(responseClubs)
+    console.log(dataClubs);
+    for (const premierClubs of dataClubs){
+        let clubsContainer = document.createElement("li");
+        clubsContainer.classList.add('nav-item');
+        clubsContainer.innerHTML = `
+            <a target="_blank" class="nav-link nav__clubs" href="${premierClubs.team.clubpage}"><img style="width: 30px" src="${premierClubs.team.logo.href}"></a>
+        `
+        document.getElementById("premierLeagueClubsId").append(clubsContainer)
+    }
+}
+const premierLeagueAwards = async () => {
+    let responseAwards = await fetch("./premier-league-awards.json")
+    let dataAwards = await responseAwards.json()
+    console.log(responseAwards)
+    console.log(dataAwards);
+    for (const premierAwards of dataAwards){
+        let awardsContainer = document.createElement("div");
+        awardsContainer.classList.add('mt-1');
+        awardsContainer.classList.add('col-6');
+        awardsContainer.classList.add('p-0');
+        awardsContainer.innerHTML = `
+        <div class="card border-0" style="width: 13rem; color: #3b0541;
+            background: url(../img/background-premier-cup.png);
+            background-size: cover";
+            background-position: center;>
+            <img src="${premierAwards.plimg}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="m-0 fs-6">${premierAwards.plaward}</p>
+                <h5 class="card-title mb-1 fs-6">${premierAwards.plname}</h5>
+                <p class="card-text">${premierAwards.plstat1}</p>
+                <p class="card-text">${premierAwards.plstat2}</p>
+                <p class="card-text">${premierAwards.plstat3}</p>
+            </div>
+        </div>
+        `
+        document.getElementById("premierLeagueCards").append(awardsContainer);
+    }
+}
+premierLeagueAwards()
+premierLeagueClubs()
+premierLeagueNews()
+//API Premier League (Table)
+let matches = document.getElementById("matches");
+let urlApi = "https://api-football-standings.azharimm.site/leagues/eng.1/standings?season=2021&sort=asc";
+fetch(urlApi)
+.then(res => res.json())
+.then((json) => {
+    for (const club of json.data.standings){
+        let trApi = document.createElement("tr");
+        trApi.innerHTML = `
+            <td class="premierPosition">${club.stats[8].displayValue}</td>
+            <td class="premierClub d-flex flex-row"><img class="img-fluid" src="${club.team.logos[0].href}" alt=""><span>${club.team.shortDisplayName}</span></td>
+            <td class="premierStats">${club.stats[3].displayValue}</td>
+            <td class="premierStats">${club.stats[0].displayValue}</td>
+            <td class="premierStats">${club.stats[2].displayValue}</td>
+            <td class="premierStats">${club.stats[1].displayValue}</td>
+            <td class="premierStats">${club.stats[4].displayValue}</td>
+            <td class="premierStats">${club.stats[5].displayValue}</td>
+            <td class="premierStats">${club.stats[9].displayValue}</td>
+            <td class="premierStats--modifier">${club.stats[6].displayValue}</td>
+        `
+        document.getElementById("matches").append(trApi);
+    }
+});
